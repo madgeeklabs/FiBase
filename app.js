e0e83e444c35c5364a2674b259da07260888a897
Alejandro Vidal Rodriguez <goofyahead@gmail.com>
2014-09-23 17:34:13 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var bodyParser  = require('body-parser');
+
+server.listen(3000);
+
+app.use( bodyParser.json() ); 
+app.get('/', function (req, res) {
+	res.sendfile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+	console.log('connected someone');
+	
+	socket.on('newnews', function (data) {
+		socket.broadcast.emit('news', { picture: 'http://1.bp.blogspot.com/_xoP2ezvpU7I/TOpwOt_abqI/AAAAAAAAAoI/gJZ8nRSZkoU/s1600/zoidberg.png',
+		text: 'Zoidberg is da best' });
+		console.log(data);
+	});
+});
+
+app.get('/listinng', function (req, res) {
+	console.log(req.body);
+});
